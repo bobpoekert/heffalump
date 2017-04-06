d910cd281392aad50f4014ada546c9911ca1c921
Bob Poekert <bob@poekert.com>
2017-04-06 19:45:43 +0000

    lein new

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to heffalump
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/heffalump/core_test.clj
@@ -0,0 +1,7 @@
+(ns heffalump.core-test
+  (:require [clojure.test :refer :all]
+            [heffalump.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
