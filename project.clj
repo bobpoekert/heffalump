3f62ba6b9ceab0c6163efb3ae2d6f805567d8df6
Bob Poekert <bob@poekert.com>
2017-04-06 22:05:04 -0700

    update deps

--- project.clj
+++ project.clj
@@ -3,4 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [
+    [org.clojure/clojure "1.8.0"]
+    [aleph "0.4.3"]
+    [compojure "1.5.2"]
+    [com.google.guava/guava "21.0"]
+    [org.clojure/java.jdbc "0.7.0-alpha3"]
+    [org.apache.derby/derby "10.13.1.1"]
+])
